0e9bb4b0ea807279b0e84af58a50f05f374ec440
Fazouane Marouane <fazouanem3@gmail.com>
2018-10-28 23:30:38 +0100

    Alternative/Faster implementation using ES6 Sets

--- index.js
+++ index.js
@@ -2,85 +2,33 @@
 
 var toPosInt = require("es5-ext/number/to-pos-integer");
 
-var LinkedList = function() {
-	this.firstNode = this.lastNode = undefined;
-	this.keymap = Object.create(null);
-	this.size = 0;
-};
-
-LinkedList.prototype.removeNode = function(node, updateKeymap) {
-	if (node.next) {
-		node.next.previous = node.previous;
-	}
-	if (node.previous) {
-		node.previous.next = node.next;
-	}
-	if (node === this.lastNode) {
-		this.lastNode = node.previous;
-		if (updateKeymap) {
-			delete this.keymap[node.key];
-			this.size--;
-		}
-	}
-	node.next = node.previous = undefined;
-};
-
-LinkedList.prototype.appendToHead = function(node, updateKeymap) {
-	if (this.firstNode) {
-		node.next = this.firstNode;
-		this.firstNode.previous = node;
-	} else {
-		this.lastNode = node;
-	}
-	this.firstNode = node;
-	if (updateKeymap) {
-		this.keymap[node.key] = node;
-		this.size++;
-	}
-};
-
-LinkedList.prototype.clear = function() {
-	this.firstNode = this.lastNode = undefined;
-	this.keymap = Object.create(null);
-	this.size = 0;
-};
-
-LinkedList.prototype.getNode = function(key) {
-	return this.keymap[key];
-};
-
-LinkedList.prototype.getSize = function() {
-	return this.size;
-};
-
 module.exports = function(limit) {
-	var list = new LinkedList();
-	limit = toPosInt(limit);
-
+	var queue = new Set(),
+		lastKey = undefined,
+		limit = toPosInt(limit);
 	return {
-		hit: function(key) {
-			var node = list.getNode(key);
-			if (!node) {
-				list.appendToHead({ key: key }, true);
-			} else if (node !== list.firstNode) {
-				list.removeNode(node, false);
-				list.appendToHead(node, false);
+		hit: function(id) {
+			if (lastKey === id) return;
+			lastKey = id;
+			if (queue.delete(id)) {
+				queue.add(id);
+				return;
 			}
-			if (list.getSize() > limit) {
-				var keyToremove = list.lastNode.key;
-				list.removeNode(list.lastNode, true);
-				return keyToremove;
+			queue.add(id);
+			if (queue.size > limit) {
+				var base = queue.keys().next().value;
+				queue.delete(base);
+				return base;
 			}
-			return undefined;
 		},
-		delete: function(key) {
-			var node = list.getNode(key);
-			if (node) {
-				list.removeNode(node, true);
+		delete: function(id) {
+			if (lastKey === id) {
+				lastKey = undefined;
 			}
+			queue.delete(id);
 		},
 		clear: function() {
-			list.clear();
+			queue.clear();
 		}
 	};
 };
