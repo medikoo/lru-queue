a92ea629c385ccd5f158f29307b0b38cfbd1a54d
Fazouane Marouane <fazouanem3@gmail.com>
2018-10-28 23:27:40 +0100

    Alternative implementation using linked lists

--- index.js
+++ index.js
@@ -2,54 +2,85 @@
 
 var toPosInt = require("es5-ext/number/to-pos-integer");
 
-var create = Object.create;
+var LinkedList = function() {
+	this.firstNode = this.lastNode = undefined;
+	this.keymap = Object.create(null);
+	this.size = 0;
+};
 
-module.exports = function (limit) {
-	var size = 0,
-		base = 1,
-		queue = create(null),
-		map = create(null),
-		index = 0,
-		del;
+LinkedList.prototype.removeNode = function(node, updateKeymap) {
+	if (node.next) {
+		node.next.previous = node.previous;
+	}
+	if (node.previous) {
+		node.previous.next = node.next;
+	}
+	if (node === this.lastNode) {
+		this.lastNode = node.previous;
+		if (updateKeymap) {
+			delete this.keymap[node.key];
+			this.size--;
+		}
+	}
+	node.next = node.previous = undefined;
+};
+
+LinkedList.prototype.appendToHead = function(node, updateKeymap) {
+	if (this.firstNode) {
+		node.next = this.firstNode;
+		this.firstNode.previous = node;
+	} else {
+		this.lastNode = node;
+	}
+	this.firstNode = node;
+	if (updateKeymap) {
+		this.keymap[node.key] = node;
+		this.size++;
+	}
+};
+
+LinkedList.prototype.clear = function() {
+	this.firstNode = this.lastNode = undefined;
+	this.keymap = Object.create(null);
+	this.size = 0;
+};
+
+LinkedList.prototype.getNode = function(key) {
+	return this.keymap[key];
+};
+
+LinkedList.prototype.getSize = function() {
+	return this.size;
+};
+
+module.exports = function(limit) {
+	var list = new LinkedList();
 	limit = toPosInt(limit);
+
 	return {
-		hit: function (id) {
-			var oldIndex = map[id];
-			if (oldIndex === index) return undefined;
-			var nuIndex = ++index;
-			queue[nuIndex] = id;
-			map[id] = nuIndex;
-			if (!oldIndex) {
-				++size;
-				if (size <= limit) return undefined;
-				id = queue[base];
-				del(id);
-				return id;
+		hit: function(key) {
+			var node = list.getNode(key);
+			if (!node) {
+				list.appendToHead({ key: key }, true);
+			} else if (node !== list.firstNode) {
+				list.removeNode(node, false);
+				list.appendToHead(node, false);
 			}
-			delete queue[oldIndex];
-			if (base !== oldIndex) return undefined;
-			while (!hasOwnProperty.call(queue, ++base)) continue; // jslint: ignore
+			if (list.getSize() > limit) {
+				var keyToremove = list.lastNode.key;
+				list.removeNode(list.lastNode, true);
+				return keyToremove;
+			}
 			return undefined;
 		},
-		delete: (del = function (id) {
-			var oldIndex = map[id];
-			if (!oldIndex) return;
-			delete queue[oldIndex];
-			delete map[id];
-			--size;
-			if (base !== oldIndex) return;
-			if (!size) {
-				index = 0;
-				base = 1;
-				return;
+		delete: function(key) {
+			var node = list.getNode(key);
+			if (node) {
+				list.removeNode(node, true);
 			}
-			while (!hasOwnProperty.call(queue, ++base)) continue; // jslint: ignore
-		}),
-		clear: function () {
-			size = index = 0;
-			base = 1;
-			queue = create(null);
-			map = create(null);
+		},
+		clear: function() {
+			list.clear();
 		}
 	};
 };
